7d779dafbabc37604d0dea406876d4c5fcb6976a
gototakuma <gota.main@i.softbank.jp>
2019-05-16 15:04:35 +0000

    Second day

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "ユーザの新規作成に成功しました。"
       redirect_to @user
     else
       render 'new'
@@ -20,6 +21,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name,:email,:pasword,:password_confirmation)
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 end
