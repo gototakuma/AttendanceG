07c8ba1059bb1143b9997ccc23f2223783b79cf6
gototakuma <gota.main@i.softbank.jp>
2019-05-18 08:22:10 +0000

    Five-day

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "管理者",
+             email: "email@sample.com",
+             belongs: "社長",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+
+59.times do |n|
+  name  = Faker::Name.name
+  email = "email#{n+1}@sample.com"
+  belongs = "営業部(#{n+1})"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               belongs: belongs,
+               password:              password,
+               password_confirmation: password)
+end
