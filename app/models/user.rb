49ee3db63403269866163446b0aeb2db0cc34017
gototakuma <gota.main@i.softbank.jp>
2019-05-26 03:49:31 +0000

    ValidatesJP

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  has_secure_password(validations: false)
-  validates :password,presence: true, length: {minimum: 6 },allow_nil: true
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6 },allow_nil: true
 end
--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module AttendanceG
     config.load_defaults 5.1
     config.time_zone = 'Tokyo'
     Faker::Config.locale = :ja
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
