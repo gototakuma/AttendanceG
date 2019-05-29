ed3230fe0d648e232871864c43d4f272ec709fa3
gototakuma <gota.main@i.softbank.jp>
2019-05-29 08:57:20 +0000

    AttendanceFinished2

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,12 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   validates :worked_on, presence: true
-end
+  
+  
+  validate :finished_at_st_blank
+    def finished_at_st_blank
+      if started_at.blank? && finished_at.present?
+       errors.add(:finished_at, "出勤時間がありません。")
+      end
+    end
+end  
