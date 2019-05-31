ed3e2b6183df06fc84fdc60691bb8d69a471615a
gototakuma <gota.main@i.softbank.jp>
2019-05-31 07:46:53 +0000

    protection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index,:show,:edit,:update,:destroy]
-  before_action :correct_user, only: [:edit,:update]
+  before_action :correct_user, only: [:show,:edit,:update]
   before_action :admin_user, only: [:destroy,:edit_basic_info,:update_basic_info]
   
   def show
@@ -97,7 +97,10 @@ class UsersController < ApplicationController
   
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
+    unless current_user?(@user) 
+      flash[:danger] = "他人のデータは観覧できません"
+      redirect_to(root_url) 
+    end
   end
   
   def admin_user
