fdc06fd805d13cac0d353f9695005f3d58aca63e
gototakuma <gota.main@i.softbank.jp>
2019-06-11 07:14:01 +0000

    ModalPetition4

--- app/controllers/over_times_controller.rb
+++
@@ -1,7 +0,0 @@
-class OverTimesController < ApplicationController
-  def update
-  end
-
-  def edit
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'over_times/update'
-
-  get 'over_times/edit'
-
   get 'sessions/new'
 
   root 'first#home'
@@ -15,7 +11,6 @@ Rails.application.routes.draw do
   patch 'update-basic-info', to: 'users#update_basic_info'
   get 'users/:id/attendances/:date/edit', to: 'attendances#edit', as: :edit_attendances
   patch 'users/:id/attendances/:date/update', to: 'attendances#update', as: :update_attendances  
-  get 'users/:id/attendances/show', to: 'attendances#show', as: :show
   resources :users do
     resources :attendances, only: :create
   end
