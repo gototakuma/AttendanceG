aabbb7a9732808dd71f7aec16ff49f8970708b86
gototakuma <gota.main@i.softbank.jp>
2019-06-28 04:07:29 +0000

    OvertimeCount2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @users = User.all
-    @count = Attendance.where(instructor_name: params[:instructor_name]).where(approval: "申請中").count
+    @count = Attendance.where(instructor_name: current_user.id).where(approval: "申請中").count
     @instructor = User.where(instructor: true)
     if params[:ftime].nil?
       @ftime = Date.today.beginning_of_month
--- db/migrate/20190628005916_add_instructor_name_to_attendances.rb
+++ db/migrate/20190628005916_add_instructor_name_to_attendances.rb
@@ -1,5 +1,5 @@
 class AddInstructorNameToAttendances < ActiveRecord::Migration[5.1]
   def change
-    add_column :attendances, :instructor_name, :string
+    add_column :attendances, :instructor_name, :integer
   end
 end
